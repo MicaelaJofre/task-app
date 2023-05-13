815718cc3af454d33af70ca7c6a4e8dc19459c43
Micaela Analia Jofre <mkjofre@gmail.com>
2023-05-13 00:18:03 -0300

    Created link from detailtask

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import notTask from "../../public/img/delete.png"
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,17 +16,19 @@ if(tasks.length === 0){
 }
   return (
     <main className={`flex min-h-screen justify-center p-24 ${inter.className}`}>
-      <section className='flex justify-between max-w-7xl flex-wrap'>
+      <section className='flex justify-between max-w-7xl flex-wrap gap-10'>
         {
           tasks.map(task=>{
             return(
               <div key={task._id} className="max-w-sm w-[250px] max-h-sm h-[160px] rounded overflow-hidden shadow-lg">
                 <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">{task.title}</div>
+                  <div className="font-bold text-xl mb-2 truncate">{task.title}</div>
                     <p className="text-gray-700 text-base truncate">{task.content}</p>
                 </div>
                 <div className="px-6 pt-4 pb-2">
-                  <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Ver</button>
+                  <Link href={`task/${task._id}`}>
+                    <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Ver</button>
+                  </Link>                  
                   <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Editar</button>
                 </div>
               </div>
