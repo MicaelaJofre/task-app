0c22e65232e4b67f85a9299fd66d6e369f86d4d1
Micaela Analia Jofre <mkjofre@gmail.com>
2023-05-13 00:17:25 -0300

    Created link from menu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,19 @@
 'use client'
 import Image from "next/image"
 import task from "../../public/img/outbox.png"
+import Link from 'next/link';
 
 const Navbar = () => {
 
 
   return (
     <div className="bg-zinc-800 text-white flex items-center justify-between px-6 py-3">
-      <Image alt={'newTask'} width={40} height={40} src={task} />
-      <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Crear nueva tarea</button>
+      <Link href='/'>
+        <Image alt={'newTask'} width={40} height={40} src={task} />
+      </Link>      
+      <Link href='/task/NewTask'>
+        <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Crear nueva tarea</button>
+      </Link>
     </div>
   )
 }
