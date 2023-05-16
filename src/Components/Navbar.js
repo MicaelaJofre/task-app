f07bd834a2dd7f3ffd05c62bb17154e97401ad1d
Micaela Analia Jofre <mkjofre@gmail.com>
2023-05-15 22:20:59 -0300

    Change image

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 'use client'
 import Image from "next/image"
-import task from "../../public/img/outbox.png"
+import task from "../../public/img/outbox.jpg"
 import Link from 'next/link';
 
 const Navbar = () => {
@@ -9,10 +9,10 @@ const Navbar = () => {
   return (
     <div className="bg-zinc-800 text-white flex items-center justify-between px-6 py-3">
       <Link href='/'>
-        <Image alt={'newTask'} width={40} height={40} src={task} />
+        <Image className="rounded-full focu: border-none" alt={'newTask'} width={40} height={40} src={task} />
       </Link>      
       <Link href='/task/NewTask'>
-        <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Crear nueva tarea</button>
+        <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-cyan-100">Crear nueva tarea</button>
       </Link>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import notTask from "../../public/img/delete.png"
+import notTask from "../../public/img/toDo.jpg"
 import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,14 +9,14 @@ export default function Home({tasks}) {
 if(tasks.length === 0){
   return(
   <div className={`flex flex-col min-h-screen justify-center items-center gap-5 ${inter.className}`}>
-    <p className='font-bold uppercase text-gray-800'>No hay tareas</p>
-    <Image alt={'notTask'} width={150} height={150} src={notTask}/>
+    <p className='font-bold uppercase text-cyan-600'>¡No tienes tareas pendientes!</p>
+    <Image className='w-auto md:h-auto h-60' alt={'notTask'} width={350} height={350} src={notTask}/>
   </div>
   )
 }
   return (
-    <main className={`flex min-h-screen justify-center p-24 ${inter.className}`}>
-      <section className='flex justify-between max-w-7xl flex-wrap gap-10'>
+    <main className={`flex min-h-screen p-24 relative ${inter.className}`}>
+      <section className='flex max-w-7xl flex-wrap gap-10'>
         {
           tasks.map(task=>{
             return(
@@ -27,9 +27,11 @@ if(tasks.length === 0){
                 </div>
                 <div className="px-6 pt-4 pb-2">
                   <Link href={`task/${task._id}`}>
-                    <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Ver</button>
-                  </Link>                  
-                  <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Editar</button>
+                    <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-cyan-100">Ver</button>
+                  </Link>   
+                  <Link href={`task/${task._id}/edit`}>
+                    <button className="inline-block cursor-pointer bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-cyan-100">Editar</button>
+                  </Link>               
                 </div>
               </div>
             )
